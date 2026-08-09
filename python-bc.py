9df3f0d4c7f1dee83480e29dce91e1ebd7844c91
Hareesh Gunji <gunjihareesh767@gmail.com>
2026-08-09 08:49:30 +0530

    Update AWS Secrets Manager secret name and host

--- python-bc.py
+++ python-bc.py
@@ -11,7 +11,7 @@ CORS(app)
 # ===============================
 # AWS Secrets Manager Configuration
 # ===============================
-SECRET_NAME = "rds!db-1c9f0269-b88b-48c6-8ce3-8107093c70d3"     # Your Secret Name
+SECRET_NAME = "rds!db-6490e6ec-45ef-4f16-9dae-7543419df6d1"     # Your Secret Name
 REGION_NAME = "us-east-1"
 
 # ===============================
@@ -42,7 +42,7 @@ def get_db_connection():
     password = get_db_password()
 
     return mysql.connector.connect(
-        host="database-1.cnreuurazt3c.us-east-1.rds.amazonaws.com",
+        host="database-1.cct206s06do4.us-east-1.rds.amazonaws.com",
         user="admin",
         password=password,
         database="dev",
